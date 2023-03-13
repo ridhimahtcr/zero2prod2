5220bf59a1539ae39ca0d82f89ba62369493501c
Ridhima Khyalappa <ridhima_khyalappa@heartcore.co.jp>
2023-03-13 11:19:36 +0900

    updates

--- src/main.rs
+++ src/main.rs
@@ -4,44 +4,31 @@ use std::net::TcpListener;
 use zero2prod2::configuration::get_configuration;
 use zero2prod2::startup::run;
 use sqlx::PgPool;
-use tracing::subscriber::set_global_default;
-use tracing_bunyan_formatter::{BunyanFormattingLayer, JsonStorageLayer};
-use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
-use tracing_log::LogTracer;
 use zero2prod2::telemetry::{get_subscriber, init_subscriber};
+use secrecy::Secret;
 use secrecy::ExposeSecret;
-
+use sqlx::postgres::PgPoolOptions;
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
 
-    let subscriber = get_subscriber("zero2prod2".into(), "info".into(), std::io::stdout
+    let subscriber = get_subscriber(
+        "zero2prod2".into(), "info".into(), std::io::stdout
     );
 
+    init_subscriber(subscriber);
 
-    LogTracer::init().expect("Failed to set logger");
-
-    let env_filter = EnvFilter::try_from_default_env()
-        .unwrap_or(EnvFilter::new("info"));
-    let formatting_layer = BunyanFormattingLayer::new(
-        "zero2prod2".into(),
-        std::io::stdout
-    );
-
-    let subscriber = Registry::default()
-        .with(env_filter)
-        .with(JsonStorageLayer)
-        .with(formatting_layer);
-
-    set_global_default(subscriber).expect("Failed to set subscriber");
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool = PgPool::connect(
-        &configuration.database.connection_string().expose_secret())
-        .await
-        .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let connection_pool = PgPoolOptions::new()
+        .acquire_timeout(std::time::Duration::from_secs(2))
+                                                                   .connect_lazy(
+        &configuration.database.connection_string().expose_secret()
+    )
+        .expect("Failed to create Postgres connection pool.")
+        ;
+    let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
     run(listener, connection_pool)?.await?;
     Ok(())
--- src/startup.rs
+++ src/startup.rs
@@ -2,14 +2,15 @@
 use crate::routes::{subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
-use sqlx::PgPool;
+use sqlx::{PgPool, Pool, Postgres};
 use std::net::TcpListener;
 use crate::health_check;
 use actix_web::web::Data;
+use sqlx::pool::PoolOptions;
 use tracing_actix_web::TracingLogger;
 
 pub fn run(
-    listener: TcpListener, db_pool: PgPool
+    listener: TcpListener, db_pool: Pool<Postgres>
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
